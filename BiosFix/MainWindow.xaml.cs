e2e9336bacb1e389a87818bdb60f9d9cdfecb7d7
Kieran Wynne <keewynne90@gmail.com>
2020-08-25 11:42:56 +0100

    Bound errors to interface

--- BiosFix/MainWindow.xaml.cs
+++ BiosFix/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using MahApps.Metro.Controls;
 
@@ -11,83 +12,101 @@ namespace BiosFix
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///
+
     public partial class MainWindow
     {
-        public MainWindow()
+
+        public static readonly DependencyProperty CurrentStatus = 
+            DependencyProperty.Register("State",typeof(string), typeof(MainWindow));
+
+        public string State
+        {
+            get => this.GetValue(CurrentStatus) as string;
+            set => this.SetValue(CurrentStatus, value);
+        }
+
+    public MainWindow()
         {
             this.DataContext = this;
             if (!File.Exists("DnSmbios64.exe"))
             {
-                MessageBox.Show("Unable to find required DNSMBios executable");
-                //throw new DnSmbiosNotFound();
+                State = "Unable to find required DNSMBios executable";
             }
 
             if (!File.Exists("UsrIdent64.exe"))
             {
-                MessageBox.Show("Unable to find required UsrIdent64.exe executable");
-                //throw new UsrIdent64NotFound();
+                State = "Unable to find required UsrIdent64.exe executable";
             }
         }
 
         private void Write_OnClick(object sender, RoutedEventArgs e)
         {
-            var TextDoc = new List<string>();
+            var textDoc = new List<string>();
             
             foreach (var elem in FileText.GetChildObjects())
             {
-               
-                if (elem.GetType().ToString() == "System.Windows.Documents.Run")
+                switch (elem.GetType().ToString())
                 {
-                    var run = elem as Run; 
-                    TextDoc.Add(run.Text);
-                }
-                else if (elem.GetType().ToString() == "System.Windows.Documents.LineBreak")
-                {
-                    TextDoc.Add(Environment.NewLine);
+                    case "System.Windows.Documents.Run":
+                    {
+                        var run = elem as Run; 
+                        textDoc.Add(run?.Text);
+                        break;
+                    }
+                    case "System.Windows.Documents.LineBreak":
+                        textDoc.Add(Environment.NewLine);
+                        break;
                 }
             }
-            var TextRaw = String.Join(String.Empty, TextDoc);
+            var textRaw = String.Join(string.Empty, textDoc);
             File.Delete("SMBIOIN.txt");
-            File.WriteAllText("SMBIOIN.txt", TextRaw);
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = true;
-            startInfo.Verb = "runas";
-            startInfo.FileName = @"DnSmbios64.exe";
-            startInfo.Arguments = "FieldService";
-            startInfo.RedirectStandardOutput = true;
+            File.WriteAllText("SMBIOIN.txt", textRaw);
+            var startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                Verb = "runas",
+                FileName = @"DnSmbios64.exe",
+                Arguments = "FieldService",
+                RedirectStandardOutput = true
+            };
             var process = Process.Start(startInfo);
-            Output.Text = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
+            Output.Text = process?.StandardOutput.ReadToEnd();
+            process?.WaitForExit();
         }
         
         private void ExecuteChecksumRepair_OnClick(object sender, RoutedEventArgs e)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.Verb = "runas";
-            startInfo.CreateNoWindow = true;
-            startInfo.FileName = @"DnSmbios64.exe";
-            startInfo.Arguments = "ChecksumRepair";
-            startInfo.RedirectStandardOutput = true;
+            var startInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                CreateNoWindow = true,
+                FileName = @"DnSmbios64.exe",
+                Arguments = "ChecksumRepair",
+                RedirectStandardOutput = true
+            };
 
             var process = Process.Start(startInfo);
-            Output.Text = process.StandardOutput.ReadToEnd();
+            Output.Text = process?.StandardOutput.ReadToEnd();
 
-            process.WaitForExit();
+            process?.WaitForExit();
         }
         
         private void ExecuteStructRepair_OnClick(object sender, RoutedEventArgs e)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.Verb = "runas";
-            startInfo.CreateNoWindow = true;
-            startInfo.FileName = @"DnSmbios64.exe";
-            startInfo.Arguments = "StructRepair";
-            startInfo.RedirectStandardOutput = true;
+            var startInfo = new ProcessStartInfo
+            {
+                Verb = "runas",
+                CreateNoWindow = true,
+                FileName = @"DnSmbios64.exe",
+                Arguments = "StructRepair",
+                RedirectStandardOutput = true
+            };
 
             var process = Process.Start(startInfo);
-            Output.Text = process.StandardOutput.ReadToEnd();
+            Output.Text = process?.StandardOutput.ReadToEnd();
 
-            process.WaitForExit();
+            process?.WaitForExit();
         }
         
     }
@@ -99,13 +118,13 @@ namespace BiosFix
 
     public class DnSmbiosNotFound : Exception
     {
-        public override string Message => $"DnSmbios not found. Drop into {System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)} or add to path";
+        public override string Message => $"DnSmbios not found. Drop into {System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()?.Location)} or add to path";
     }
 
     public class UsrIdent64NotFound : Exception
     {
         public override string Message =>
-            $"UsrIdent64 not found. Drop into {System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)} or add to path";
+            $"UsrIdent64 not found. Drop into {System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()?.Location)} or add to path";
     }
 
 }
